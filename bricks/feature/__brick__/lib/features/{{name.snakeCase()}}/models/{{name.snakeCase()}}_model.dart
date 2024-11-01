6d564df20fd4466a78a4a3634eec6cc741c271a8
joshuapurbaa <yoshuapurba06@gmail.com>
2024-11-01 21:04:48 +0700

    fix

--- bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/models/{{name.snakeCase()}}_model.dart
+++ bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/models/{{name.snakeCase()}}_model.dart
@@ -1,11 +1,29 @@
 class {{name.pascalCase()}} {
-  {{name.pascalCase()}}();
+  {{name.pascalCase()}}(
+    {
+      required this.id,
+      required this.updatedAt,
+      required this.createdAt,
+    }
+  );
+
+  final String id;
+  final String updatedAt;
+  final String createdAt;
 
   factory {{name.pascalCase()}}.fromMap(Map<String, dynamic> map) {
-    return {{name.pascalCase()}}();
+    return {{name.pascalCase()}}(
+      id: map['id'] as String,
+      updatedAt: map['updated_at'] as String,
+      createdAt: map['created_at'] as String,
+    );
   }
 
   Map<String, dynamic> toMap() {
-    return {};
+    return {
+      'id': id,
+      'updated_at': updatedAt,
+      'created_at': createdAt,
+    };
   }
 }
